1b9fb26c1f34aba21a24fb1edd15f81b9074e694
qc90 <qc90@foxmail.com>
2020-01-08 18:43:00 +0800

    update register page

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,47 +1,48 @@
 <template>
-<div class="team-update-component">
-  <div class="team-update">
-	<ValidationObserver v-slot="{ invalid }">
-	  <van-cell-group title="类型" :border="false">
-		<ValidationProvider v-slot="{ errors }" rules="required" name="type">
-			<van-field
-			v-model="registerForm.type"
-			readonly
-			clickable
-			:error-message="errors[0]"
-			placeholder="请选择DID类型"
-			@focus="showPicker = true"/>
-		</ValidationProvider>
-	  </van-cell-group>
-	  <van-cell-group title="公钥" :border="false">
-	  		<ValidationProvider v-slot="{ errors }" name="pubkey">
-	  		  <van-field v-model="registerForm.pubkey"
-	  		  :error-message="errors[0]"
-	  		  placeholder="请输入您的公钥" />
-	  		</ValidationProvider>
-	  </van-cell-group>
-	  <!-- <van-cell-group title="上级" :border="false">
-		<ValidationProvider v-slot="{ errors }" name="pubkeysuperior">
-		  <van-field v-model="registerForm.pubkeysuperior"
-		  :error-message="errors[0]"
-		  placeholder="请输入您的上级" />
-		</ValidationProvider>
-	  </van-cell-group> -->
-	  <van-button type="primary" square
-	  class="btn-submit"
-	  size="large"
-	  :disabled="invalid" @click="handleSubmit">确认生成</van-button>
-	</ValidationObserver>
-  </div>
-  <van-popup v-model="showPicker" position="bottom">
-    <van-picker
-      show-toolbar
-      :columns="columns"
-      @cancel="showPicker = false"
-      @confirm="onConfirm"
-    />
-  </van-popup>
-</div>
+	<div class="register-component">
+	  <div class="register">
+		<ValidationObserver v-slot="{ invalid }">
+		  <van-cell-group title="类型" :border="false">
+			<ValidationProvider v-slot="{ errors }" rules="required" name="type">
+				<van-field
+				v-model="registerForm.type"
+				readonly
+				clickable
+				:error-message="errors[0]"
+				placeholder="请选择DID类型"
+				@focus="showPicker = true"/>
+			</ValidationProvider>
+		  </van-cell-group>
+		  <van-cell-group title="公钥" :border="false">
+				<ValidationProvider v-slot="{ errors }" name="pubkey">
+				  <van-field v-model="registerForm.pubkey"
+				  :error-message="errors[0]"
+				  placeholder="请输入您的公钥" />
+				</ValidationProvider>
+		  </van-cell-group>
+		  <!-- <van-cell-group title="上级" :border="false">
+			<ValidationProvider v-slot="{ errors }" name="pubkeysuperior">
+			  <van-field v-model="registerForm.pubkeysuperior"
+			  :error-message="errors[0]"
+			  placeholder="请输入您的上级" />
+			</ValidationProvider>
+		  </van-cell-group> -->
+		  <van-button type="primary" square
+		  class="btn-submit"
+		  size="large"
+		  :disabled="invalid" @click="handleSubmit">确认生成</van-button>
+		</ValidationObserver>
+	  </div>
+	  <van-popup v-model="showPicker" position="bottom">
+		<van-picker
+		  show-toolbar
+		  :columns="columns"
+		  @cancel="showPicker = false"
+		  @confirm="onConfirm"
+		/>
+	  </van-popup>
+	  <van-panel v-if="did" title="您的DID" status="成功" :desc="did"/>
+	</div>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -50,7 +51,7 @@ import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import { DID_TYPE } from '@/util/chain'
 import { encodeAddress } from '@polkadot/util-crypto'
 import { stringToHex } from '@polkadot/util'
-import { didToHex } from '@/util/common'
+import { didToHex, hexToDid } from '@/util/common'
 export default {
 	name: 'register',
 	data() {
@@ -61,6 +62,7 @@ export default {
 				superior: '',
 				pubkey: ''
 			},
+			did: '',
 			showPicker: false,
 			columns
 		}
@@ -71,6 +73,10 @@ export default {
 	},
 	mounted() {
 		this.$store.commit('hideLoading')
+		this.sockets.subscribe('Created', payload => {
+			const [didHash] = JSON.parse(payload.msg)
+			this.did = hexToDid(didHash)
+		})
 	},
 	methods: {
 		async handleSubmit() {
@@ -98,3 +104,11 @@ export default {
 	}
 }
 </script>
+<style lang="scss">
+	@import '../assets/css/variables.scss';
+	.register-component {
+		.van-panel {
+			margin-top: $largeGutter;
+		}
+	}
+</style>
