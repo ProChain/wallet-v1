d452616ac6477101318b46b1f509e820b6cce760
qc90 <qc90@foxmail.com>
2020-04-15 22:04:23 +0800

    update url

--- src/router/index.js
+++ src/router/index.js
@@ -226,7 +226,7 @@ router.beforeEach(async(to, from, next) => {
 			})
 		}
 	} else if (to.name === 'walletHome' && window.location.href.indexOf('code') === -1) {
-		const redirectUri = encodeURIComponent('http://wallet.prabox.net')
+		const redirectUri = encodeURIComponent('https://wallet.prabox.net')
 		window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx48f51627bef8bcdf&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
 	} else {
 		next()
